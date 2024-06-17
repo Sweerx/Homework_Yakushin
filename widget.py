b52b5435e0007bdf781a48cf9e6ba330552e96bb
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-06-18 00:34:25 +0300

    wrote a function mask_account_card

--- widget.py
+++ widget.py
@@ -1,5 +1,50 @@
-def mask_account_card():
-    pass
+def mask_account_card(text: str) -> str:
+    """Функция принимает строку с информацией — тип карты/счета и номер карты/счета
+    Возвращать исходную строку с замаскированным номером карты/счета."""
+    new_text_list = text.split()
+    if len(new_text_list) > 2:
+        num_card_str = new_text_list[2]
+        num_card_one_block = num_card_str[:4]
+        num_card_two_block = num_card_str[4:8].replace(num_card_str[6:8], "*" * 2)
+        num_card_three_block = num_card_str[8:12].replace(num_card_str[8:12], "*" * 4)
+        num_card_four_block = num_card_str[12:16]
+        num_mask_card_len_3 = (
+            new_text_list[0]
+            + " "
+            + new_text_list[1]
+            + " "
+            + num_card_one_block
+            + " "
+            + num_card_two_block
+            + " "
+            + num_card_three_block
+            + " "
+            + num_card_four_block
+        )
+        return num_mask_card_len_3
+    elif new_text_list[0] != "Счет":
+        num_card_str = new_text_list[1]
+        num_card_one_block = num_card_str[:4]
+        num_card_two_block = num_card_str[4:8].replace(num_card_str[6:8], "*" * 2)
+        num_card_three_block = num_card_str[8:12].replace(num_card_str[8:12], "*" * 4)
+        num_card_four_block = num_card_str[12:16]
+        num_mask_card = (
+            new_text_list[0]
+            + " "
+            + num_card_one_block
+            + " "
+            + num_card_two_block
+            + " "
+            + num_card_three_block
+            + " "
+            + num_card_four_block
+        )
+    else:
+        num_account_str = new_text_list[1]
+        num_mask_account = num_account_str[-6:].replace(num_account_str[-6:-4], "*" * 2)
+        return new_text_list[0] + " " + num_mask_account
+
+    return num_mask_card
 
 
 def get_data():
