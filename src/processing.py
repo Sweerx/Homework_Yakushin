fc0caf6c61bf4f68e7b7c4a6c572bf5b7a5b2140
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-06-23 00:51:30 +0300

    create func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,11 @@ def filter_by_state(list_key: list, state: str = "EXECUTED") -> list:
         if el.get("state") == state:
             filter_list.append(el)
     return filter_list
+
+
+def sort_by_date(date: list, reverse_key: bool = True) -> list:
+    """Функция которая принимает на вход список словарей и возвращает новый список,
+    в котором исходные словари отсортированы по убыванию даты (ключ date).
+    Функция принимает два аргумента, второй необязательный задает порядок сортировки (убывание, возрастание)."""
+    sort_date = sorted(date, key=lambda dict: dict["date"], reverse=reverse_key)
+    return sort_date
