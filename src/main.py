5b804bcc7576da641fdd11be6664dd0a765df67d
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-06-23 00:31:16 +0300

    fixed main and processing, used flake8, black, mypy

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from src.processing import filter_for_state
 from src.widget import get_data, mask_account_card
 
 if __name__ == "__main__":
@@ -8,3 +9,12 @@ if __name__ == "__main__":
     print(mask_account_card("Счет 35383033474447895560"))
 
     print(get_data("2018-07-11T02:26:18.671407"))
+
+list_one = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+print(filter_for_state(list_one))
+print(filter_for_state(list_one, "CANCELED"))
--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
-def filter_for_state(list_key: list, state="EXECUTED") -> dict:
-    '''Функция принимает на вход список словарей и значение для ключа state
+def filter_for_state(list_key: list, state: str = "EXECUTED") -> list:
+    """Функция принимает на вход список словарей и значение для ключа state
     (опциональный параметр со значением по умолчанию EXECUTED)
     и возвращает новый список, содержащий только те словари,
-    у которых ключ state содержит переданное в функцию значение.'''
+    у которых ключ state содержит переданное в функцию значение."""
     filter_list = []
     for el in list_key:
-        if el.get('state') == state:
+        if el.get("state") == state:
             filter_list.append(el)
     return filter_list
