c61d3bf4849d0031d8cefbed63c719c8b29d6021
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-06-23 00:28:15 +0300

    fixed func filter_for_state in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,10 @@
-def list_for_state():
-    pass
+def filter_for_state(list_key: list, state="EXECUTED") -> dict:
+    '''Функция принимает на вход список словарей и значение для ключа state
+    (опциональный параметр со значением по умолчанию EXECUTED)
+    и возвращает новый список, содержащий только те словари,
+    у которых ключ state содержит переданное в функцию значение.'''
+    filter_list = []
+    for el in list_key:
+        if el.get('state') == state:
+            filter_list.append(el)
+    return filter_list
