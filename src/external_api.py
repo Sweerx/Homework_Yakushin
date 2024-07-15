3363f15a9eb78e190ab05747845f2984e022db67
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-07-16 01:20:18 +0300

    fixed main add func in external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,20 @@
-import requests
 import os
+from typing import Any
+
+import requests
 from dotenv import load_dotenv
+
+
+def sum_transaction_amount(user_transaction: dict) -> Any:
+    currency = user_transaction["operationAmount"]["currency"]["code"]
+    amount = user_transaction["operationAmount"]["amount"]
+    if currency in ["USD", "EUR"]:
+        load_dotenv(".env")
+        api_key = os.getenv("API_KEY")
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}&from={currency}&amount={amount}"
+        headers = {"apikey": api_key}
+        response = requests.get(url, headers=headers)
+        result = response.json()["result"]
+        return round(result, 2)
+    else:
+        return amount
